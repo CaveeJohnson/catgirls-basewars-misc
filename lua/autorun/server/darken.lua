fcb891f164dff990ff81081271ecdef4d146b63f
Callum Slaney-Flett <csf@zsbk.dev>
2024-06-02 13:23:02 +0100

    less spooky

--- lua/autorun/server/darken.lua
+++ lua/autorun/server/darken.lua
@@ -1,3 +1,3 @@
-hook.Add("InitPostEntity", "darken_da_map", function()
-    engine.LightStyle(0, "b")
-end)
+-- hook.Add("InitPostEntity", "darken_da_map", function()
+--     engine.LightStyle(0, "b")
+-- end)
