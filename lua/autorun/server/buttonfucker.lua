10994dc83319401aa3922633ac74f6af51292c50
Callum Slaney-Flett <csf@zsbk.dev>
2024-06-03 17:24:17 +0100

    feat: remove exploitable doors

---
+++ lua/autorun/server/buttonfucker.lua
@@ -0,0 +1,42 @@
+if game.GetMap() ~= "gm_excess_construct_13" then return end
+
+local buttons = {
+	[1] = 3366,
+	[2] = 1687,
+	[3] = 1719,
+	[4] = 1704,
+	[5] = 3301,
+	[6] = 2223,
+	[7] = 2224,
+	[8] = 3299
+}
+
+local doors = {
+	[1] = 3365,
+	[2] = 1702,
+	[3] = 1686,
+	[4] = 1718,
+	[5] = 1703,
+	[6] = 3300,
+	[7] = 2218,
+	[8] = 2219,
+	[9] = 3279
+}
+
+local function buttonfucker()
+    for _, v in ipairs(buttons) do
+        local e = ents.GetMapCreatedEntity(v)
+        if IsValid(e) then SafeRemoveEntity(e) end
+    end
+
+    for _, v in ipairs(doors) do
+        local e = ents.GetMapCreatedEntity(v)
+        if IsValid(e) then e:Fire("open") end
+    end
+end
+
+local tag = "buttonfucker"
+hook.Add("InitPostEntity", tag, buttonfucker)
+hook.Add("PostCleanupMap", tag, buttonfucker)
+
+buttonfucker()
